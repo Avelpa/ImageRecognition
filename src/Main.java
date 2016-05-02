29091871352e5597a4e357db72e93ed0d311093c
kobed6328 <kobed6328@HRH-IC0027290.wrdsb.ca>
2016-05-02 14:24:38 -0400

    learning

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.Scanner;
 
@@ -26,25 +27,25 @@ public class Main {
         Symbol[] symbols = reader.getSymbols(testImg);
         
         doMath(symbols, reader);
-        /*
-        BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_BGR);
-        int[] data = new int[10000];
-        int i = 0;
-        for (int y = 0; y < 100; y ++)
-        {
-            for (int x = 0; x < 100; x ++)
-            {
-                if ((x+y)%2 == 0)
-                    data[i] = Color.BLACK.getRGB();
-                else
-                    data[i] = Color.WHITE.getRGB();
-                i ++;
-            }
-        }
         
-        img.setRGB(0, 0, 100, 100, data, 0, 100);
-        
-        FileManager.saveImage(img, "images/newthing.png");*/
+//        BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_BGR);
+//        int[] data = new int[10000];
+//        int i = 0;
+//        for (int y = 0; y < 100; y ++)
+//        {
+//            for (int x = 0; x < 100; x ++)
+//            {
+//                if ((x+y)%2 == 0)
+//                    data[i] = Color.BLACK.getRGB();
+//                else
+//                    data[i] = Color.WHITE.getRGB();
+//                i ++;
+//            }
+//        }
+//        
+//        img.setRGB(0, 0, 100, 100, data, 0, 100);
+//        
+//        FileManager.saveImage(img, "images/newthing.png");
     }
     
     private static void printSyms(Symbol[] syms, String delimeter)
